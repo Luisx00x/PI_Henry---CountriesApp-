5e335fa550499d3eaabedfd37a3ec517d0a0cf9d
Luis Marcano <lmarcano203@gmail.com>
2023-04-13 16:43:09 -0500

    mod: createStore for legacy_createStore

--- PI-Countries-main/client/src/redux/store/index.js
+++ PI-Countries-main/client/src/redux/store/index.js
@@ -1,9 +1,9 @@
-import {legacy_createStore as createStore, applyMiddleware, compose} from 'redux';
+import {legacy_createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';    //Para trabajo asincrono
 import reducer from '../reducer';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+const store = legacy_createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
