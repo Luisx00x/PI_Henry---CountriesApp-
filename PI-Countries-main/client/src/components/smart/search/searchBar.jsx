d4352b196991ca88b386d115cfdc9488ca8ec6d5
Luis Marcano <lmarcano203@gmail.com>
2023-06-23 13:08:12 +0200

    features: first version of search autocomplete

--- PI-Countries-main/client/src/components/smart/search/searchBar.jsx
+++ PI-Countries-main/client/src/components/smart/search/searchBar.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { useDispatch } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import s from './searchBar.module.css';
 import {resetPag, searchID} from '../../../redux/actions';
 import { useHistory, useLocation } from 'react-router-dom';
@@ -7,7 +7,18 @@ import { useHistory, useLocation } from 'react-router-dom';
 export default function SearchBar (props){
 
   const [input, setInput] = useState("");
+  const [result, setResult] = useState([]);
   const dispatch = useDispatch();
+
+  const allCountries = useSelector(state => state.allCountries)
+  
+  useEffect( () => {
+    
+    let result = allCountries.filter( ele => ele.name.toLowerCase().includes(input.toLocaleLowerCase()))
+    setResult( prev => {
+      return [...result]
+    })
+  },[input, allCountries])
 
   const handleInput = (e) => {
     setInput(e.target.value);
@@ -17,25 +28,38 @@ export default function SearchBar (props){
 
   const history = useHistory();
 
-  console.log(location)
-
   return (
-    <form onSubmit={(e) =>{
-      e.preventDefault();
-      dispatch(searchID(location.search))
-      setInput("");
-      dispatch(resetPag())
-    }}>
-        <input 
-        type="text"
-        placeholder="Nombre de país..."
-        onChange={handleInput}
-        value = {input}
-        />
-        <input type="submit" value="Buscar" className={s.button} onClick = { () => {
-         if(input) history.push({search: `name=${input}`})
-         else history.push()
-        }} />
-    </form>
+    <>
+      <form onSubmit={(e) =>{
+        e.preventDefault();
+        dispatch(searchID(location.search))
+        setInput("");
+        dispatch(resetPag())
+      }}>
+          <input 
+          type="search"
+          name="searchCountry"
+          list="countriesList"
+          placeholder="Nombre de país..."
+          onChange={handleInput}
+          value = {input}
+          />
+          <input type="submit" value="Buscar" className={s.button} onClick = { () => {
+          if(input) history.push({search: `name=${input}`})
+          else history.push()
+          }} />
+      </form>
+
+      <datalist id="countriesList">
+        {
+            result?.map( country => {
+            return <option key={country.name} value={country.name}/>
+          })
+          
+        }
+      </datalist>
+    </>
+
+    
   );
 }
