c9c9835d2905b3af85a2dc056b8659b3d82c1f4e
Luis Marcano <lmarcano203@gmail.com>
2023-06-27 14:20:29 -0500

    fix: searchBar now is feching information to db

--- PI-Countries-main/client/src/components/smart/search/searchBar.jsx
+++ PI-Countries-main/client/src/components/smart/search/searchBar.jsx
@@ -3,25 +3,41 @@ import { useDispatch, useSelector } from 'react-redux';
 import s from './searchBar.module.css';
 import {resetPag, searchID} from '../../../redux/actions';
 import { useHistory, useLocation } from 'react-router-dom';
+import { BACK_URL } from '../../../App';
 
 export default function SearchBar (props){
 
   const [input, setInput] = useState("");
   const [result, setResult] = useState([]);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchController = new AbortController();
 
   const allCountries = useSelector(state => state.allCountries)
   
   useEffect( () => {
-    
-    let result = allCountries.filter( ele => ele.name.toLowerCase().includes(input.toLocaleLowerCase()))
-    setResult( prev => {
-      return [...result]
-    })
+      if(isLoading){
+        try{
+          fetch(`${BACK_URL}/home/?name=${input}`,
+          {
+            signal: fetchController.signal
+          })
+          .then(res => res.json())
+          .then(res => {
+            setResult( prev => [...res]);
+            setIsLoading(false);
+          })
+        }catch(err){
+          
+        } 
+      };
   },[input, allCountries])
+  
+  console.log(result)
 
   const handleInput = (e) => {
     setInput(e.target.value);
+    setIsLoading( prev => true)
   }
 
   const location = useLocation();
@@ -35,26 +51,40 @@ export default function SearchBar (props){
         dispatch(searchID(location.search))
         setInput("");
         dispatch(resetPag())
-      }}>
+      }}
+      className={s.container}
+      >
           <input 
           type="search"
-          name="searchCountry"
           list="countriesList"
           placeholder="Nombre de país..."
-          onChange={handleInput}
-          value = {input}
+          onChange={(e) => {
+            handleInput(e)
+            fetchController.abort();
+          }}
+          vlue = {input}
+          className={s.input}
           />
+           
+          <div className={s.imgContainer}>
+            {isLoading ? <img className={s.img} src="./img/VAyR.gif" alt="loading" /> : null}
+          </div>
+
           <input type="submit" value="Buscar" className={s.button} onClick = { () => {
           if(input) history.push({search: `name=${input}`})
           else history.push()
-          }} />
+          }}
+          />
+
       </form>
+
+      {console.log(isLoading)}
 
       <datalist id="countriesList">
         {
-            result?.map( country => {
+            !isLoading ? result?.map( country => {
             return <option key={country.name} value={country.name}/>
-          })
+          }) : null
           
         }
       </datalist>
